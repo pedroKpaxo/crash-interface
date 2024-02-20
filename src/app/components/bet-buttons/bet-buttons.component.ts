b3d2b2db094134c2dfc95d42ce9bbf235fc07a95
pedrokpaxo <pedrograxxa@gmail.com>
2024-02-19 23:34:49 -0300

    Update bet controllers

--- src/app/components/bet-buttons/bet-buttons.component.ts
+++ src/app/components/bet-buttons/bet-buttons.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-bet-buttons',
   templateUrl: './bet-buttons.component.html',
-  styleUrls: ['./bet-buttons.component.scss']
+  styleUrls: ['./bet-buttons.component.scss'],
 })
 export class BetButtonsComponent {
+  hasSecond = false;
+  @Input() isMain = true;
+  @Output() addController = new EventEmitter();
+  constructor() {}
 
+  toggleAddController() {
+    this.addController.emit();
+  }
 }
--- src/app/components/bet-spinner/bet-spinner.component.ts
+++ src/app/components/bet-spinner/bet-spinner.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-bet-spinner',
   templateUrl: './bet-spinner.component.html',
-  styleUrls: ['./bet-spinner.component.scss']
+  styleUrls: ['./bet-spinner.component.scss'],
 })
-export class BetSpinnerComponent {
+export class BetSpinnerComponent implements OnInit {
+  @ViewChild('input', { static: true }) input!: ElementRef<HTMLInputElement>;
 
+  value = 1.0;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.input.nativeElement.value = '1.0';
+  }
+  /**
+   * Increase the value of the spinner by 0.1
+   * and maintain the value to 2 decimal places
+   */
+  increaseValue() {
+    this.value = Math.round((this.value + 0.1) * 100) / 100;
+    this.setValue();
+  }
+
+  /**
+   * Decrease the value of the spinner by 0.1
+   */
+  decreaseValue() {
+    this.value = Math.round((this.value - 0.1) * 100) / 100;
+    this.setValue();
+  }
+
+  setValue() {
+    this.input.nativeElement.value = String(this.value);
+  }
 }
